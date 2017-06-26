7324775422e1e486986b8cc8629ee44c9d5383f8
Solamosso Arsene <a.solamosso@outlook.com>
2017-06-26 23:06:49 +0200

    WordWrap kata
    WordWrap kata in C#

---
+++ WordWrapKata/WordWraptests.cs
@@ -0,0 +1,55 @@
+﻿using NFluent;
+using NUnit.Framework;
+
+namespace WordWrapKata
+{
+    public class WordWraptests
+    {
+        [Test]
+        public void should_returnaaa_when_5()
+        {
+            var actual = Wrapper.Wrap("aaa", 5);    
+            Check.That(actual).IsEqualTo("aaa");
+        }
+
+        [Test]
+        public void should_return_aaa_bs_aa_when_input_is_aaa_s_aa()
+        {
+            
+            var actual = Wrapper.Wrap("aaa aa",3);
+            Check.That(actual).IsEqualTo("aaa\naa");
+        }
+
+        [Test]
+        public void should_returnabsabsaa_when_input_is_3()
+        {
+            var actual = Wrapper.Wrap("a aaa", 3);
+            Check.That(actual).IsEqualTo("a\naaa");
+        }
+
+        [Test]
+        public void should_return_wrapped_text_when_colunm_number_is_at_the_same_position_as_the_space()
+        {
+            var actual = Wrapper.Wrap("aaaaa",3);
+            Check.That(actual).IsEqualTo("aaa\naa");
+        }
+
+
+        [TestCase("aaaaaa bbbb cccc",6, "aaaaaa\nbbbb\ncccc")]
+        [TestCase("aaaaaa bbbb cccc",6, "aaaaaa\nbbbb\ncccc")]
+        [TestCase("aaaaaa bbbb cccc",6, "aaaaaa\nbbbb\ncccc")]
+        [TestCase("aaaaaa bbbb cccc",6, "aaaaaa\nbbbb\ncccc")]
+        [TestCase("aaaaaa bbbb cccc",6, "aaaaaa\nbbbb\ncccc")]
+        [TestCase("aaa bbb", 6, "aaa\nbbb")]
+        [TestCase("aaa bbbb", 6, "aaa\nbbbb")]
+        [TestCase("aaa bbbb", 7, "aaa\nbbbb")]
+        [TestCase("aaaaaa bbbbcccc", 6, "aaaaaa\nbbbbcc\ncc")]
+        [TestCase("aaaaaa bbbb cccc",6, "aaaaaa\nbbbb\ncccc")]
+        public void should_break_input_when_input_contains_severals_words_with_len_less_than_column_number0(string input, int columnNumber, string expected)
+        {
+            var actual = Wrapper.Wrap(input, columnNumber);
+            Check.That(actual).IsEqualTo(expected);
+        }
+
+    }
+}
---
+++ WordWrapKata/Wrapper.cs
@@ -0,0 +1,33 @@
+﻿namespace WordWrapKata
+{
+    public class Wrapper
+    {
+        public static string Wrap(string inputText, int columnNumber)
+        {
+            string result = "";
+            string currentText = inputText;
+            var breakPosition = columnNumber;
+            if (string.IsNullOrEmpty(currentText))
+                return currentText;
+
+            if (inputText.Length <= columnNumber)
+                return inputText.Trim();
+
+            var indexOf = inputText.IndexOf(" ");
+
+            if (indexOf != -1 && indexOf <= columnNumber)
+            {
+                breakPosition = indexOf;
+            }
+            else if (indexOf <= -1)
+            {
+                breakPosition = columnNumber;
+            }
+
+            result += currentText.Trim().Substring(0, breakPosition) + "\n" + Wrap(currentText.Trim().Substring(breakPosition), columnNumber);
+            result = result.Trim();
+            return result;
+        }
+
+    }
+}
